74c6eabd18d94b8b991323a96306d35b98750f47
DanielVafidis <daniel.vafidis@gmail.com>
2019-02-07 16:08:20 +0100

    test source

---
+++ src/containers/Projects/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Projects.scss';
+const Projects = () => {
+    return (
+        <section name="Projects" className="section projects">My projects</section>
+    );
+}
+
+export default Projects;
